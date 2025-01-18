bf5c7c54f457cc4867b501fbbbb2f60440d9692b
ChefGusta <gustavo.alberto.cortes@gmail.com>
2025-01-18 00:26:33 -0500

    save the bandwidth

--- src/main/java/frc/robot/subsystems/EndEffectorSubsystem.java
+++ src/main/java/frc/robot/subsystems/EndEffectorSubsystem.java
@@ -72,6 +72,5 @@ public class EndEffectorSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    setSpeed(targetSpeed); 
   }
 }
