c96e2708d6287aa07954d5739358b55e86d1d3e8
ChefGusta <gustavo.alberto.cortes@gmail.com>
2025-01-19 21:04:38 -0500

    javadocs

--- src/main/java/frc/robot/subsystems/Watchdog.java
+++ src/main/java/frc/robot/subsystems/Watchdog.java
@@ -6,9 +6,14 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
-/** Add your docs here. */
 public class Watchdog {
 
+    /**
+     * start and end refer to the bounds of the system needing the watchdog (ex. the
+     * min and max angle of a Pivot)
+     * the current refers to the current position of the system (ex. for pivot it
+     * would be the current angle)
+     */
     private double start, end;
     private DoubleSupplier current;
 
@@ -20,8 +25,8 @@ public class Watchdog {
 
     /**
      * 
-     * @returns whether or not bounds were crossed (ex. if Pivot is out of set min
-     *          or max)
+     * @returns whether or not bounds were crossed (ex. will return true if
+     *          everything is good)
      *          fixes wrapping issue with many pivoting systems (ex. if Pivot
      *          wrapped around from 360 -> 0, basic watchdog code would break)
      */
