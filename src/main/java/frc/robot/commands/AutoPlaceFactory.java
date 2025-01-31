58f75f0110559f70a00618b2f0bca81877a829a4
ChefGusta <gustavo.alberto.cortes@gmail.com>
2025-01-30 19:03:16 -0500

    2nd commit added deploy to floor intake

--- src/main/java/frc/robot/commands/AutoPlaceFactory.java
+++ src/main/java/frc/robot/commands/AutoPlaceFactory.java
@@ -5,11 +5,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.drive.PlaceCommand;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.EndEffectorSubsystem;
-import frc.robot.Constants.Elevator.Position;
+import frc.robot.Constants.Elevator.Position;   
 
 /** Add your docs here. */
 public class AutoPlaceFactory {
@@ -23,9 +23,10 @@ public class AutoPlaceFactory {
 
     public Command getAutoPlace(Position targetPosition){
         return new SequentialCommandGroup(
-            
+            new DeployFloorIntakeCommand(Rotation2d.fromDegrees(61), elevatorSubsystem, null, endEffectorSubsystem), // todo: figure out what resting angle should be at
             new ElevatorCommand(elevatorSubsystem, targetPosition),
             new PlaceCommand(endEffectorSubsystem)
         );
     }
 }
+    
