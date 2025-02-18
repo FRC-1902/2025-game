5a109b038f601edce6da83b45c15de8f62d50565
ChefGusta <gustavo.alberto.cortes@gmail.com>
2025-02-17 23:08:14 -0500

    Continuous Conditional

---
+++ src/main/java/frc/robot/commands/ContinuousConditionalCommand.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import java.util.function.BooleanSupplier;
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class ContinuousConditionalCommand extends Command {
+  Command incomingCommand, baseCommand;
+  BooleanSupplier supplier;
+  /** Creates a new ContinuousConditionalCommand. */
+  public ContinuousConditionalCommand(Command incomingCommand, Command baseCommand, BooleanSupplier supplier) {
+  this.supplier = supplier;
+  this.incomingCommand = incomingCommand;
+  this.baseCommand = baseCommand;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(supplier.getAsBoolean()){
+      baseCommand.cancel();
+      incomingCommand.schedule();
+    }
+    else{
+      incomingCommand.cancel();
+      baseCommand.schedule();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    incomingCommand.cancel();
+    baseCommand.cancel();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return baseCommand.isFinished() || incomingCommand.isFinished();
+  }
+}
