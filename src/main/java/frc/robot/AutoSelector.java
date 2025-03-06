b5307b2e2f646155f74f44a423edf816ed42b595
ChefGusta <gustavo.alberto.cortes@gmail.com>
2025-03-06 17:46:05 -0500

    the Everything App

--- src/main/java/frc/robot/AutoSelector.java
+++ src/main/java/frc/robot/AutoSelector.java
@@ -1,29 +1,42 @@
 package frc.robot;
 
+import java.lang.reflect.Field;
 import java.util.Optional;
 
+import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+
+import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.AutoPlaceFactory;
 import frc.robot.commands.ContinuousConditionalCommand;
+import frc.robot.commands.ElevatorCommand;
 import frc.robot.commands.ElevatorFactory;
 import frc.robot.commands.PlaceCommand;
 import frc.robot.commands.drive.ObjectAlign;
+import frc.robot.commands.intake.AutoIntakeFactory;
 import frc.robot.commands.intake.DeployFloorIntakeCommand;
 import frc.robot.subsystems.AlgaeIntakeSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.EndEffectorSubsystem;
 import frc.robot.subsystems.FloorIntakeSubsystem;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
 import frc.robot.subsystems.vision.DetectionSubsystem;
+import frc.robot.FieldConstants;
+import frc.robot.Constants.EndEffector;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 
 /*
  * Publishes a network table chooser to smart dashboard to select the autonomous command. 
@@ -37,19 +50,24 @@ public class AutoSelector {
   FloorIntakeSubsystem floorIntake;
   EndEffectorSubsystem endEffector;
   DetectionSubsystem detectionSubsystem;
+  ElevatorSubsystem elevator;
 
   DeployFloorIntakeCommand deployFloorIntakeCommand;
   AutoPlaceFactory autoPlaceFactory;
+  AutoIntakeFactory autoIntakeFactory;
   ElevatorFactory elevatorFactory;
+  ObjectAlign objectAlign;
 
   ContinuousConditionalCommand continuousConditionalCommand;
   
-  public AutoSelector(RobotContainer robotContainer, ContinuousConditionalCommand continuousConditionalCommand) {
+  public AutoSelector(RobotContainer robotContainer, ContinuousConditionalCommand continuousConditionalCommand, ObjectAlign objectAlign) {
     swerve = robotContainer.swerve;
+    elevator = robotContainer.elevator;
     algaeIntake = robotContainer.algaeIntake;
     floorIntake = robotContainer.floorIntake;
     endEffector = robotContainer.endEffector;
     detectionSubsystem = robotContainer.detectionSubsystem;
+    this.objectAlign = objectAlign;
     this.continuousConditionalCommand = continuousConditionalCommand;
 
     autoChooser = new LoggedDashboardChooser<>("Auto Chooser");
@@ -75,7 +93,7 @@ public class AutoSelector {
     }
   }
 
-  private Command setStartPosition(double x, double y) {
+  public Command setStartPosition(double x, double y) {
     return new ConditionalCommand(
       new SequentialCommandGroup(
         new InstantCommand(() -> swerve.resetOdometry(new Pose2d(x, y, Rotation2d.fromDegrees(180)))), // TODO: Check if correct
@@ -89,7 +107,7 @@ public class AutoSelector {
     );
   }
 
-  private Command getPieceRoutine(String PathName){
+  private Command getVerified(String PathName){
     return new ContinuousConditionalCommand(
       swerve.getFollowPathCommand(PathName),
       new ObjectAlign(detectionSubsystem, swerve),
@@ -127,21 +145,100 @@ public class AutoSelector {
     );
   }
 
-  //
-  private SequentialCommandGroup getSpecialAuto(){
-    return new SequentialCommandGroup(
-      // setup odometry
-      setStartPosition(20, 10),
-      // drive to reef
-      new ParallelCommandGroup(
-        elevatorFactory.getElevatorCommand(Constants.Elevator.Position.L3),
-        swerve.getFollowPathCommand("1.path")
-      ),
-      new PlaceCommand(endEffector),
-      new ParallelCommandGroup(
-        elevatorFactory.getElevatorCommand(Constants.Elevator.Position.MIN),
-        getPieceRoutine("gussy is special")
-      )
-    );
-  }
-}
+  
+  private SequentialCommandGroup getTheEverythingApp(){
+      return new SequentialCommandGroup(
+        // setup odometry
+        setStartPosition(7.170, 2.200),
+        // drive and suck
+        new ParallelCommandGroup(
+          elevatorFactory.getElevatorCommand(Constants.Elevator.Position.L3),
+          swerve.getFollowPathCommand("3 L3 1")
+        ),
+        // XXX: remove algae
+        // Place
+        new PlaceCommand(endEffector),
+        // Drive to HP
+
+        // XXX: deadline with intaking
+        new ParallelCommandGroup(
+          getVerified("3 L3 2"), 
+          elevatorFactory.getElevatorCommand(Constants.Elevator.Position.MIN)
+        ),
+        // Intake
+        new ConditionalCommand(
+          autoIntakeFactory.getIntakeSequence(Constants.FloorIntake.HP_ANGLE), /// 
+          autoIntakeFactory.getIntakeSequence(Constants.FloorIntake.FLOOR_ANGLE), 
+          () -> !objectAlign.isScheduled()), ///
+        new WaitCommand(3), // XXX: why?
+        // Drive to reef
+        new ParallelCommandGroup(
+          elevatorFactory.getElevatorCommand(Constants.Elevator.Position.L3),
+          swerve.getFollowPathCommand("3 L3 3")
+        ),
+        // XXX: remove algae
+        // Place 
+        new PlaceCommand(endEffector),
+
+        /// end system 1
+        new ParallelCommandGroup(
+          getVerified("3 L3 4"),
+          elevatorFactory.getElevatorCommand(Constants.Elevator.Position.MIN)
+        ),
+        new ConditionalCommand(
+          autoIntakeFactory.getIntakeSequence(Constants.FloorIntake.HP_ANGLE), //
+          autoIntakeFactory.getIntakeSequence(Constants.FloorIntake.FLOOR_ANGLE), //
+          () -> !objectAlign.isScheduled()), //
+        new ParallelCommandGroup(
+          elevatorFactory.getElevatorCommand(Constants.Elevator.Position.L3),
+          swerve.getFollowPathCommand("3 L3 5")
+        ),
+        new PlaceCommand(endEffector)
+      );
+    }
+
+    private SequentialCommandGroup getBluesky(){
+        return new SequentialCommandGroup(
+          // setup odometry
+          setStartPosition(7.170, 2.200),
+          // drive and suck
+          new ParallelCommandGroup(
+            elevatorFactory.getElevatorCommand(Constants.Elevator.Position.L3),
+            swerve.getFollowPathCommand("3 L3 1")
+            //new AlgaeIntakeCommand(algaeIntake)
+          ),
+          // Place
+          new PlaceCommand(endEffector),
+          // Drive to HP
+          new ParallelCommandGroup(
+            swerve.getFollowPathCommand("some path that is essentially fencesitting"),
+            elevatorFactory.getElevatorCommand(Constants.Elevator.Position.MIN)
+          ),
+          // Intake
+          //new AlgaeOuttakeCommand(algaeIntake),
+          new ObjectAlign(detectionSubsystem, swerve), // allign
+          autoIntakeFactory.getIntakeSequence(Constants.FloorIntake.FLOOR_ANGLE),
+          
+          new WaitCommand(3),
+          // Drive to reef
+          new ParallelCommandGroup(
+            elevatorFactory.getElevatorCommand(Constants.Elevator.Position.L3),
+            swerve.getFollowPathCommand("3 L3 3")
+          ),
+          // Place 
+          new PlaceCommand(endEffector),
+          new ParallelCommandGroup(
+            swerve.getFollowPathCommand("Some path that makes the comments angry"),
+            elevatorFactory.getElevatorCommand(Constants.Elevator.Position.MIN)
+          ),
+          new ObjectAlign(detectionSubsystem, swerve),
+          autoIntakeFactory.getIntakeSequence(Constants.FloorIntake.FLOOR_ANGLE),
+          new ParallelCommandGroup(
+            elevatorFactory.getElevatorCommand(Constants.Elevator.Position.L3),
+            swerve.getFollowPathCommand("3 L3 5")
+          ),
+          new PlaceCommand(endEffector)
+        );
+      }
+    }
+  
