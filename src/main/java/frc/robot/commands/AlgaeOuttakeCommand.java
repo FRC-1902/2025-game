4807b42eda80dbc4df27f3fe54d6a6138c37e0db
ChefGusta <gustavo.alberto.cortes@gmail.com>
2025-03-06 18:53:19 -0500

    STYLE: got rid of unecessary imports

--- src/main/java/frc/robot/commands/AlgaeOuttakeCommand.java
+++ src/main/java/frc/robot/commands/AlgaeOuttakeCommand.java
@@ -1,11 +1,7 @@
 package frc.robot.commands;
 
-import org.ejml.equation.Sequence;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.subsystems.AlgaeIntakeSubsystem;
 
--- src/main/java/frc/robot/subsystems/FloorIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/FloorIntakeSubsystem.java
@@ -35,8 +35,7 @@ public class FloorIntakeSubsystem extends SubsystemBase {
   private PIDController pid;
   private Alert pivotAlert;
   private final ElevatorSubsystem elevatorSubsystem;
-  private Watchdog pivotWatchdog; 
-  private Pose3d intakePose;
+  private Watchdog pivotWatchdog;
 
   /** Creates a new FloorIntake. */
   public FloorIntakeSubsystem(ElevatorSubsystem elevatorSubsystem) {
@@ -88,7 +87,6 @@ public class FloorIntakeSubsystem extends SubsystemBase {
 
     // resetSafeParameters might be an issue
     pivotMotor.configure(pivotConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-
     rollerMotor.configure(rollerConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
   }
 
@@ -157,6 +155,18 @@ public class FloorIntakeSubsystem extends SubsystemBase {
     }
   }
 
+  private void setupLogging(){
+    SmartDashboard.putData("PID/FloorIntake", pid); // TODO: Remove after tuning
+    SmartDashboard.putNumber("FloorIntake/Current Angle", getAngle().getDegrees());
+    SmartDashboard.putBoolean("FloorIntake/Piece Sensor", irSensorActive());
+    SmartDashboard.putBoolean("FloorIntake/atSetpoint", pid.atSetpoint());
+    SmartDashboard.putNumber("FloorIntake/power", pivotMotor.get());
+
+    Logger.recordOutput("FloorIntake/Intake Pose", 
+      new Pose3d(new Translation3d(0, 0, 0), new Rotation3d(0, getAngle().getDegrees(), 0))
+    ); // TODO: Math and offset
+  }
+
   @Override
   public void periodic() {
 
@@ -168,14 +178,8 @@ public class FloorIntakeSubsystem extends SubsystemBase {
     double power = pid.calculate(getAngle().getDegrees())
         + Constants.FloorIntake.PIVOT_G * Math.cos(getAngle().getRadians() + Rotation2d.fromDegrees(4).getRadians());
 
-    intakePose = new Pose3d(new Translation3d(0, 0, 0), new Rotation3d(0, getAngle().getDegrees(), 0)); // TODO: Math and offset
 
-    SmartDashboard.putData("PID/FloorIntake", pid); // TODO: Remove after tuning
-    SmartDashboard.putNumber("FloorIntake/Current Angle", getAngle().getDegrees());
-    SmartDashboard.putBoolean("FloorIntake/Piece Sensor", irSensorActive());
-    SmartDashboard.putBoolean("FloorIntake/atSetpoint", pid.atSetpoint());
-    SmartDashboard.putNumber("FloorIntake/power", power);
-    Logger.recordOutput("FloorIntake/Intake Pose", intakePose);
+    setupLogging();
 
     if (pivotWatchdog()) {
       pivotMotor.set(0);
