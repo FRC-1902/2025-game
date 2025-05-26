a6ad6f11c8d08d43d4de26c219a575119d2961ea
ChefGusta <gustavo.alberto.cortes@gmail.com>
2025-05-26 16:32:00 -0400

    FEAT: elevatorsim initial setup

--- src/main/java/frc/robot/subsystems/Elevator/ElevaterSim.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevaterSim.java
@@ -7,21 +7,39 @@ package frc.robot.subsystems.Elevator;
 import frc.robot.Robot;
 import frc.robot.Constants.Elevator.Position;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.units.measure.Distance;
 
 /** Add your docs here. */
 public class ElevaterSim implements ElevatorBase {
 
-ElevatorBaseInputs inputs; 
     Position targetPosition; 
+    boolean locked; 
     ElevatorSim elevatorSim; 
+    PIDController pid;
+    Pose3d stageOne, stageTwo; 
 
     public ElevaterSim(){
-        inputs = new ElevatorBaseInputs(); 
-        // Sim setup            
+        // Sim setup        
+        elevatorSim = new ElevatorSim(null, getPosition(), getPosition(), getPosition(), getPosition(), getPosition(), locked, getPosition(), null); 
+        
+        pid = new PIDController(getPosition(), getPosition(), getPosition()); 
+        stageOne = new Pose3d(null, null); 
+        stageTwo = new Pose3d(null, null); 
+
+        resetPID();
+        //TODO: values
     }
 
     public double getPosition(){
-       return inputs.currentPosition.getHeight();
+       return elevatorSim.getPositionMeters();
     };
 
     public void setPosition(Position position){
@@ -29,26 +47,47 @@ ElevatorBaseInputs inputs;
     };
 
     public boolean limitSwitchTriggered(){
-        return inputs.limitSwitchTriggered; 
+        return elevatorSim.wouldHitLowerLimit(getPosition()); // TODO: Some number for the lower limit of the elevator
     }; 
 
     public boolean atSetpoint(){
-        return inputs.atSetpoint; 
+        return 0.01 >= Math.abs(getPosition() - targetPosition.getHeight()); 
     };
 
-    public void resetPID(){};
+    public void resetPID(){
+        pid.reset();
+    };
 
     public boolean isLocked(){
-        return inputs.isLocked; 
+        return locked; 
     }; 
 
     public void setLocked(boolean lock){
-        inputs.isLocked = lock; 
+        locked = lock; 
     }; 
+
+    private void updateTelemetry(){
+        double restingPos = 0; 
+        double currentPos = elevatorSim.getPositionMeters(); 
+        stageOne = new Pose3d(new Translation3d(restingPos, restingPos, restingPos + currentPos), new Rotation3d()); 
+        stageTwo = new Pose3d(new Translation3d(restingPos, restingPos, restingPos + (2 * currentPos)), new Rotation3d()); 
+
+        Logger.recordOutput("stageOne", stageOne); 
+        Logger.recordOutput("stageTwo", stageTwo); 
+    }
+
+    private double pidCalc(){
+        double kGConstant = 0;
+        return pid.calculate(elevatorSim.getPositionMeters(), targetPosition.getHeight()) + kGConstant;
+    }
 
     public void update(ElevatorBaseInputs inputs){
         if(Robot.isReal()) return; 
         //Sim Logic here 
+        inputs.atSetpoint = atSetpoint(); 
+        inputs.limitSwitchTriggered = limitSwitchTriggered(); 
+        elevatorSim.setInputVoltage(pidCalc());
+        updateTelemetry();
     };
 
 }
