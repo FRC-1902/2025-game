2dfdcab993b4f80469c4d1d6470392ea6e98b729
ChefGusta <gustavo.alberto.cortes@gmail.com>
2025-07-10 16:57:19 -0400

    FEAT: initial commit

---
+++ src/main/java/frc/robot/subsystems/FloorIntake/FloorSim.java
@@ -0,0 +1,130 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.FloorIntake;
+
+import org.ironmaple.simulation.IntakeSimulation;
+import org.ironmaple.simulation.IntakeSimulation.IntakeSide;
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose3d;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import static edu.wpi.first.units.Units.Inches;
+
+/** Add your docs here. */
+public class FloorSim implements FloorBase {
+
+    IntakeSimulation intakeSim; 
+    
+    FloorBaseInputs inputs;
+    double rollerSpeed = 0;
+    double motorReduction = 1; 
+    Rotation2d targetAngle;
+    SingleJointedArmSim armSim; 
+    DCMotor gearbox;
+    PIDController pid; 
+    Pose3d intakePose; 
+
+    public FloorSim() {
+        
+        inputs = new FloorBaseInputs();
+
+        gearbox = DCMotor.getNEO(1);
+
+        armSim = new SingleJointedArmSim(
+            gearbox, 
+            FloorConstants.SimultationConstants.GEARING,
+            FloorConstants.SimultationConstants.MOMENT, 
+            FloorConstants.SimultationConstants.ARM_LENGTH,
+            FloorConstants.Positions.MIN_PIVOT.getRadians(),
+            FloorConstants.Positions.MAX_PIVOT.getRadians(), 
+            FloorConstants.SimultationConstants.SIMULATE_GRAVITY,
+            FloorConstants.Positions.DEFAULT_ANGLE.getRadians()
+        );
+
+        pid = new PIDController(FloorConstants.PIDConstants.PIVOT_P, FloorConstants.PIDConstants.PIVOT_I,
+            FloorConstants.PIDConstants.PIVOT_D);
+                //pid.disableContinuousInput(); // Makes sure that intake doesn't try to gas it through the floor
+            //pid.setTolerance(FloorConstants.Positions.TOLERANCE.getDegrees());
+                //pid.setIZone(10);
+
+        // Sim setup stuff
+        
+         /*  if (RobotContainer.MAPLESIM) {
+            intakeSim = IntakeSimulation.OverTheBumperIntake(
+                "Coral",
+                null,
+                Inches.of(5),
+                Inches.of(5),
+                IntakeSide.FRONT,
+                1
+            );
+          }
+            */
+       // intakePose = new Pose3d(new Translation3d(0, 0.0, 0), new Rotation3d(-180.0, 20.54, -180.0));
+        targetAngle = FloorConstants.Positions.DEFAULT_ANGLE; 
+         
+    }
+
+    @Override
+    public void setSpeed(double speed) {
+        rollerSpeed = speed;
+    };
+
+    @Override
+    public Rotation2d getAngle() {
+        return Rotation2d.fromRadians(armSim.getAngleRads()); 
+    };
+
+    @Override
+    public void setAngle(Rotation2d angle) {
+        targetAngle = angle;
+    };
+
+    @Override
+    public boolean hasCoral() {
+        return inputs.hasCoral;
+    };
+
+    @Override
+    public void resetPID(){
+        pid.reset();
+    };
+
+    public boolean atSetpoint(){
+       return pid.atSetpoint();
+    }
+
+    private double pidCalc(){
+        return pid.calculate(getAngle().getDegrees(), targetAngle.getDegrees())
+                + FloorConstants.PIDConstants.PIVOT_G * Math.cos(getAngle().getRadians());
+    }
+    private void updateTelemetry(){
+        intakePose = new Pose3d(new Translation3d(-0.25, 0.0, 0.15), new Rotation3d(0.0, getAngle().getRadians() * -1.0, 0.0)); 
+        Logger.recordOutput("FloorIntake/IntakePose", intakePose);
+    }
+
+    @Override
+    public void update(FloorBaseInputs inputs) {
+        if (Robot.isReal()) return;
+        // Setup Sim Logic here
+        double power = pidCalc(); 
+
+        inputs.atSetpoint = atSetpoint(); 
+        armSim.setInputVoltage((1*power + (1-1)) * 12);
+        armSim.update(0.2);
+        updateTelemetry();
+        Logger.recordOutput("FloorIntake/TargetAngle", targetAngle.getDegrees());
+        Logger.recordOutput("FloorIntake/CurrentAngle", getAngle().getDegrees()); 
+        Logger.recordOutput("FloorIntake/draw", armSim.getCurrentDrawAmps());
+        Logger.recordOutput("FloorIntake/PID", power); 
+    };
+}
---
+++ src/main/java/frc/robot/subsystems/FloorIntake/FloorSubsystem.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.FloorIntake;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.FloorIntake.FloorBase.FloorBaseInputs;
+import frc.robot.Robot;
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+@Logged
+public class FloorSubsystem extends SubsystemBase {
+
+  FloorBase floorBase;
+  FloorBaseInputs inputs;
+
+  /** Creates a new FloorSubsystem. */
+  public FloorSubsystem() {
+    inputs = new FloorBaseInputs();
+    if (Robot.isReal()) {
+      floorBase = new FloorHardware();
+    } else {
+      floorBase = new FloorSim();
+    }
+  }
+
+  public Command runRollers(double speed) {
+    return runEnd(() -> floorBase.setSpeed(speed), () -> floorBase.setSpeed(0));
+  }
+
+  public Command setPivotAngle(Rotation2d angle) {
+    return new InstantCommand(() -> floorBase.setAngle(angle));
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    floorBase.update(inputs);
+  }
+}
